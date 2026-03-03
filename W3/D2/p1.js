4db8d1eaae4846d8f0c5954c4e9695cd08f95aef
varshithym <ymvarshith@gmail.com>
2026-03-03 15:41:08 +0530

    arithmatic operators

---
+++ W3/D2/p1.js
@@ -0,0 +1,17 @@
+console.log("varshith");
+const a = 20;
+const b = 20;
+
+
+console.log(a);
+console.log(typeof(a));
+console.log("a+b: ",a+b);
+console.log("a-b: ",a-b);
+console.log("a*b: ",a*b);
+console.log("a/b: ",a/b);
+console.log("a%b: ",a%b);
+console.log("a**b: ",a**b);
+// console.log("a++: ",a++);
+// console.log("++a: ",++a);
+// console.log("a--: ",a--);
+// console.log("--a: ",--a);

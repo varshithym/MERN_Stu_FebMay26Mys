2f6f35092903e28c1a544d947ae9c4747284ba1a
varshithym <ymvarshith@gmail.com>
2026-03-03 15:41:36 +0530

    Comparison operators

---
+++ W3/D2/p2.js
@@ -0,0 +1,12 @@
+console.log("Comparison operators");
+let x = 10;
+let y = "10";
+
+console.log("x==y",x==y);
+console.log("x===y",x===y);
+console.log("x!==y",x!==y);
+console.log("x>y",x>y);
+console.log("x<y",x<y);
+console.log("x<y",x<y);
+console.log("x<=y",x<=y);
+console.log("x>=y:",x>=y);

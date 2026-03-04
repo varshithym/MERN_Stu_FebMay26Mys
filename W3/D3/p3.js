e21cdd64c3245b11f0f4bec859d79c7b7c9e15fb
varshithym <ymvarshith@gmail.com>
2026-03-04 14:28:55 +0530

    break, continue

---
+++ W3/D3/p3.js
@@ -0,0 +1,17 @@
+// break
+for (let i=0; i<5; i++){
+    if(i===2){
+       break;
+
+    }
+    console.log("i",i);
+}
+
+// continue
+for (let i=0; i<5; i++){
+    if(i===2){
+       continue;
+
+    }
+    console.log("i",i);
+}

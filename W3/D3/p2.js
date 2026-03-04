e08fa3dc4bce40df900b75b450a53ddc0623d5c5
varshithym <ymvarshith@gmail.com>
2026-03-04 14:28:21 +0530

    while loop,do while loop

---
+++ W3/D3/p2.js
@@ -0,0 +1,15 @@
+// while loop
+let count = 0;
+while(count < 5){
+    console.log("count:"+count);
+    count++;
+}
+
+// do while loop
+let value = 0;
+do{
+    console.log("value",value);
+    value++;
+
+}while(value < 3);
+

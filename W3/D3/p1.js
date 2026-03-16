c61a874a07659219e30963b637f38afe62cdea7d
varshithym <ymvarshith@gmail.com>
2026-03-16 16:33:49 +0530

    for loop

--- W3/D3/p1.js
+++ W3/D3/p1.js
@@ -6,14 +6,14 @@ for(i=0; i<n; i++) {      // forward loop
     console.log("i",i);
 
 }
-for(let i = 5; i>0; i--) {      // backward loop
-    console.log("i",i);
+// for(let i = 5; i>0; i--) {      // backward loop
+//     console.log("i",i);
 
-}
+// }
 
-for(let i=1; i<=3; i++){
-    for(let j=1; j<=2; j++){
-        console.log(`i=${i},j=${j}`);
+// for(let i=1; i<=3; i++){
+//     for(let j=1; j<=2; j++){
+//         console.log(`i=${i},j=${j}`);
 
-    }
-}
+//     }
+// }

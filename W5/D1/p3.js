d828514f7a014c599d56eb129f6d51c7b0973612
varshithym <ymvarshith@gmail.com>
2026-03-17 16:44:13 +0530

    node JS demo code with Timeout function

---
+++ W5/D1/p3.js
@@ -0,0 +1,8 @@
+console.log("Node js architecture demo");
+console.log("1.script started");
+
+setTimeout(() =>{
+    console.log("3. Timer callback finished after waiting");
+
+},10000);
+console.log("2.script continued without waiting for timer callback");

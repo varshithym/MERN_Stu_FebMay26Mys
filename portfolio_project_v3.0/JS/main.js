b5ce551593b5129fa79cee0dd45a2a8a00ca9342
varshithym <ymvarshith@gmail.com>
2026-03-30 10:47:23 +0530

    Initializes all feature functions

---
+++ portfolio_project_v3.0/JS/main.js
@@ -0,0 +1,19 @@
+/* Initializes all feature functions */
+
+document.addEventListener("DOMContentLoaded", () => {
+initScrollBar();
+initScrollSpy();
+initBackToTop();
+initSearch();
+initCount();
+initFilter();
+initSort();
+initToggleDesc();
+initTyping();
+initSkills();
+initFormSave();
+initRecent();
+initTheme();
+initGreeting();
+initGeo();
+});

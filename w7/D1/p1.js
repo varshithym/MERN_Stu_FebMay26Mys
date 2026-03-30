e9fa83891f62c3d08f046be5f02e0e70468571d1
varshithym <ymvarshith@gmail.com>
2026-03-30 10:52:51 +0530

    basics of expreesJS - setup

---
+++ w7/D1/p1.js
@@ -0,0 +1,17 @@
+// basics of expreesJS - setup
+// npm init -y
+// npm install express
+
+const express = require("express");
+// calling express() creates main application object
+// this is used to register routes and middleware
+const app = express();
+// app.get() handles GET request for a specific path
+app.get("/",function(req,res){
+    // res.send() sends a response body ends the request automatically
+    res.send("hello from express server");
+});
+// listen() starts the sever in chosen port number
+app.listen(4000,function(){
+    console.log("express serer running at http://localhost:4000");
+})

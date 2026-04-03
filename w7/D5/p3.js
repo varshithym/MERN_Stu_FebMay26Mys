31bb473139c76c7dd115fc282e43ac0b6ad4914c
varshithym <ymvarshith@gmail.com>
2026-04-03 15:10:08 +0530

    Session Security , regeneration, expiration and logout

---
+++ w7/D5/p3.js
@@ -0,0 +1,62 @@
+//Session Security , regeneration, expiration and logout
+const express = require("express");
+const session = require("express-session");
+
+const app = express();
+
+app.use(session({
+    secret:"MySecretKey",
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+        maxAge:60*60*1000,
+        httpOnly:true,
+        secure:process.env.NODE_ENV ===  "production"
+    }
+}));
+app.get("/login",function(req,res,next){//after the signin details complete, this data details we get
+    req.session.regenerate(function(error){
+        if(error){
+            return next(error);
+        }
+        req.session.user = {
+        id:201,
+        username:"varshith",
+        role:"student"
+    };
+    res.send("Session regenerated & details stored after login.");
+    });
+});
+
+app.get('/profile',function(req,res){
+    if(!req.session.user){
+        return res.status(401).json({
+            success:false,
+            message:"No active login session found."
+        });
+    }
+    res.json({
+        success:true,
+        sessionUser:req.session.user
+    });
+});
+
+app.get("/logout",function(req,res,next){
+    req.session.destroy(function(error){
+        if(error){
+            return next(error);
+        }
+        res.clearCookie("connect.sid");
+        res.send("session destroyed & cookie cleared.");
+    });
+});
+
+app.use(function(error,req,res,next){
+    res.status(500).json({
+        success:false,
+        message:error.message
+    });
+});
+app.listen(4000,function(){
+    console.log("Express session demo server running at http://localhost:4000");
+});

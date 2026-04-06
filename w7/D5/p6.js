67d79a0b329a629ef5c843b0cc20b54306141d31
varshithym <ymvarshith@gmail.com>
2026-04-06 10:25:57 +0530

    Authorization with JWT and RBAC

---
+++ w7/D5/p6.js
@@ -0,0 +1,103 @@
+//Authorization with JWT and RBAC
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+const secretKey = "monkey@123";
+
+const userToken = jwt.sign(
+    {userId:101,role:"user",email:"e@e.com"},
+    secretKey,
+    {expiresIn:"1h"} 
+);
+
+
+const managerToken = jwt.sign(
+    {userId:102,role:"manager",email:"m@e.com"},
+    secretKey,
+    {expiresIn:"1h"} 
+);
+
+const adminToken = jwt.sign(
+    {userId:103,role:"admin",email:"a@e.com"},
+    secretKey,
+    {expiresIn:"1h"} 
+);
+
+console.log("User token",userToken);
+console.log("manager token",managerToken);
+console.log("admin token",adminToken);
+
+function authenticateAccessToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Bearer token is missing.",
+    });
+  }
+  try {
+    // algorithms (verify): Only trust tokens whose alg is one of these.
+    req.user = jwt.verify(token, secretKey);
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({
+        success: false,
+        message: "Access token has expired",
+      });
+    }
+    return res.status(401).json({
+      success: false,
+      message: "Access token is invalid",
+    });
+  }
+}
+
+function requireAnyRole(allowedRoles){
+    return function(req,res,next){
+        if(!req.user){
+            return res.status(401).json({
+                success:false,
+                message:"Authentication requried"
+            });
+        }
+        if(!allowedRoles.includes(req.user.role)){
+            return res.status(403).json({
+                success:false,
+                message:"Insufficient permission"
+            });
+        }
+        next();
+    }
+}
+
+app.get("/me",authenticateAccessToken,function(req,res){
+    res.json({
+        success:true,
+        user:req.user,
+        message:"Accessing my page"
+    });
+});
+
+app.get("/admin",authenticateAccessToken,requireAnyRole(["admin"]),function(req,res){//we can use any roles afte admin
+    res.json({
+        success:true,
+        message:"admin page",
+        user:req.user
+    });
+});
+
+app.get("/profile",authenticateAccessToken,requireAnyRole(["admin","manager"]),function(req,res){
+    res.json({
+        success:true,
+        message:"Profile page",
+        user:req.user
+    });
+});
+
+app.listen(4000,function(){
+    console.log("Express session demo server running at http://localhost:4000");
+});

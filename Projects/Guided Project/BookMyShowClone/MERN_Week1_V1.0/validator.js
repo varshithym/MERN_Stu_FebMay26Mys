f7832ce27995fa4e774c786227a28bd29f450735
varshithym <ymvarshith@gmail.com>
2026-04-06 10:50:12 +0530

    callback based validation functions

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week1_V1.0/validator.js
@@ -0,0 +1,32 @@
+// callback based validation functions
+
+function validateMovieSelection(movies,movieId,callback){
+    const selectedMovie = movies.find((movie)=>movie.id === movieId);
+
+    if(!selectedMovie){
+        return callback("invalid movie. choose valid movie id",null);
+    }
+    callback(null,selectedMovie);
+}
+function validatTimeSelection(movie,selectedTime,Callback){
+    const selectedShowTime = movie.showtimes.find((show)=>show.time.toLowerCase()===selectedTime.toLowerCase);
+    if(!selectedShowTime){
+        return Callback("Invalid time slot selection. Choose a valid show time.",null);
+    }
+    Callback(null,selectedShowTime);
+        
+}
+function validateSeatCount(seatCount,Callback){
+    
+    if(!isNaN(seatCount) || seatCount <=0){
+        return Callback("Invalid seat count. enter a valid seat count.",null);
+    }
+    Callback(null,seatCount);
+    
+        
+}
+module.exports = {
+    validateMovieSelection,
+    validatTimeSelection,
+    validateSeatCount
+};

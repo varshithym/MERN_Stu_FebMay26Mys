0858fda784e212462050102382f0f4d98a0166f3
varshithym <ymvarshith@gmail.com>
2026-04-09 10:11:40 +0530

    routes created for login logout profile

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1.0/routes/authRoutes.js
@@ -0,0 +1,10 @@
+// routes created for login logout profile
+const express = require("express");
+const {authMiddleware} = require("../middleware/authMiddleware");
+const {loginUser,logoutUser,getProfile} = require("../controllers/authController");
+const router = express.Router();
+router.post("/login",loginUser);
+router.post("/logout",logoutUser);
+router.get("/profile",authMiddleware,getProfile);
+
+module.exports = router;

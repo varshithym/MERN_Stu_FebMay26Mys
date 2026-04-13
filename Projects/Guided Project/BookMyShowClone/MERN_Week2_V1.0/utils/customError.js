c7e1d2bcc8f89bc420f01e847fd134b22c8bfefb
varshithym <ymvarshith@gmail.com>
2026-04-13 10:14:53 +0530

    custom error class created for error handeling

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1.0/utils/customError.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1.0/utils/customError.js
@@ -1,8 +1,9 @@
-// custom error class created for error handeling
-class CustomError extends error {
+//Custom error class created for error handling
+class CustomError extends Error{
     constructor(message,statusCode){
         super(message);
         this.statusCode = statusCode;
     }
 }
+
 module.exports = CustomError;

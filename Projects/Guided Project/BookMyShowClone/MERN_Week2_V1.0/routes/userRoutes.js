71cb087678b0c3d1548309530ea47261c5c01469
varshithym <ymvarshith@gmail.com>
2026-04-13 10:14:34 +0530

    fetching the bookings based on userId

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1.0/routes/userRoutes.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1.0/routes/userRoutes.js
@@ -1,17 +1,22 @@
-// fetching the bookings based on userId
+// Fetching the bookings based on userId
 const express = require("express");
-const {authMiddleware} = require("../middleware/authMiddleware");
-const {bookings} = require("../controllers/bookingController");
+const { authMiddleware } = require("../middleware/authMiddleware");
+const { bookings } = require("../controllers/bookingController");
+
 const router = express.Router();
-router.get("/userId/bookings",authMiddleware,(req,res)=>{
+
+//get bookings for a specific user id
+router.get("/:userId/bookings",authMiddleware,(req,res)=>{
     const userId = Number(req.params.userId);
-    const userBookings = bookings.filter((booking)=>booking.userId === userId)
+
+    const userBookings = bookings.filter((booking)=>
+    booking.userId === userId);
 
     res.status(200).json({
         success:true,
         count: userBookings.length,
         data: userBookings
-
     });
 });
+
 module.exports = router;

31ed75a6236a6eb5cf2c2e1628f835201f3594b3
varshithym <ymvarshith@gmail.com>
2026-04-13 10:10:40 +0530

    Does the ticket booking

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1.0/controllers/bookingController.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1.0/controllers/bookingController.js
@@ -3,42 +3,46 @@ const movies = require("../data/movies");
 const CustomError = require("../utils/CustomError");
 
 const bookings = [];
-function bookigValidationHandler(req, res, next) {
-    const { movieId, showtimeId, seatCount } = req.body;
 
+function bookingValidationHandler(req,res,next){
+    const {movieId,showtimeId,seatCount} = req.body;
+
+    if (!movieId || !showtimeId || !seatCount) {
+        return next(new CustomError("movieId,showtimeId,seatCount are required",404));
+    }
+    next();
 }
-if (!movieId || !showtimeId || !seatCount) {
-    return next(new CustomError("movieId,showtimeId,seatCount are required ", 404))
-}
-next();
-function createBooking(req, res, next) {
-    try {
-        const { movieId, showtimeId, seatCount } = req.body;
-        const movie = movies.find((m) => m.id === Number(movieId));
+
+function createBooking(req,res,next){
+    try{
+        const {movieId,showtimeId,seatCount} = req.body;
+
+        const movie = movies.find((m)=>m.id === Number(movieId));
         if (!movie) {
-            return next(new CustomError("movie not found", 404));
+        return next(new CustomError("Movie not found",404));
         }
-        const showtime = movies.showtimes.find((s) => s.id === Number(showtimeId));
+        const showtime = movies.showtimes.find((s)=>s.id === Number(showtimeId));
         if (!showtime) {
-            return next(new CustomError("showtime not found", 404));
+        return next(new CustomError("showtime not found",404));
         }
-        if (showtime.seatsAvailable < Number(seatCount)) {
-            return next(new CustomError("Not enough seats available", 404));
+        if (showtime.seatsAvailable<Number(seatCount)) {
+        return next(new CustomError("Not enough seats available",404));
+        }
 
-        }
-        showtime.seatsAvailable -= Number(seatCount);
+        showtime.seatsAvailable-=Number(seatCount);
 
         const booking = {
-            id: bookings.length + 1,
+            id:bookings.length+1,
             userId: req.user.id,
             userName: req.user.name,
-            movieId: movie.id,
-            movieTitle = movie.title,
-            showtimeId: showtime.id,
+            movieId:movie.id,
+            movieTitle: movie.title,
+            showtimeId:showtime.id,
             showtime: showtime.time,
-            seatCount: Number(seatCount)
+            seatCount:Number(seatCount)
         };
         bookings.push(booking);
+
         res.status(201).json({
             success:true,
             message:"Booking created successfully.",
@@ -48,35 +52,37 @@ function createBooking(req, res, next) {
         next(error);
     }
 }
+
 function getAllBookings(req,res,next){
     try{
         res.status(200).json({
             success:true,
             count: bookings.length,
             data: bookings
-        })
+        });
     }
     catch(error){
         next(error);
     }
 }
+
 function getMyBookings(req,res,next){
     try{
         const userBookings = bookings.filter((booking)=>booking.userId === req.user.id);
         res.status(200).json({
             success:true,
-            count: bookings.length,
-            data: bookings
+            count: userBookings.length,
+            data: userBookings
         });
-        
     }
     catch(error){
         next();
     }
 }
+
 module.exports = {
-    bookigValidationHandler,
+    bookingValidationHandler,
     createBooking,
     getAllBookings,
     getMyBookings
-}
+};

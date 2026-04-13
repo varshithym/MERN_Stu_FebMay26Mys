7cba7b6f77aeb455648a7c364b3076217d96610d
varshithym <ymvarshith@gmail.com>
2026-04-13 10:15:58 +0530

    querying using conditional operators

--- W8/D1/p3.js
+++ W8/D1/p3.js
@@ -1,56 +1,67 @@
-// querying using conditional operators
+
+// Applied filters to the query using comparison operators
+// MERN_Stu_FebMay26Mys\W8\D1\P3.js
 const mongoose = require("mongoose");
-async function runFilterDemo() {
+
+const productSchema = new mongoose.Schema({
+            name: String,
+            price: Number,
+            category: String,
+            status: String
+        }); 
+
+const Product = mongoose.models.Product || mongoose.model("Product",productSchema);
+
+async function runFilterDemo(){
     try{
-        await mongoose.connect("mongodb://localhost:27017/MernTraining");
-    //to connect with db
-     console.log("MongoDB connected successfully");
-     const productsSchema = new mongoose.Schema({
-        name:String,
-        price:Number,
-        catagory:String,
-        status:String
-     });
-     const product = mongoose.models.product || mongoose.model("product",productsSchema);
-     const firstProduct = await product.create([{
-        name:"soap",
-        price:50,
-        catagory:"stationary",
-        status:true
+        
+        await mongoose.connect("mongodb://localhost:27017/merntraining");
+        console.log("MongoDB connected successfully");
 
-     },
-    {
-        name:"paste",
-        price:25,
-        catagory:"stationary",
-        status:false
+        await Product.create([
+            {name: "Book",
+            price: 300,
+            category: "Stationary",
+            status: "active"},
+            {name: "Pen",
+            price: 3000,
+            category: "Stationary",
+            status: "inactive"},
+            {name: "Laptop",
+            price: 60000,
+            category: "Electronics",
+            status: "active"},
+            {name: "CD",
+            price: 50,
+            category: "Electronics",
+            status: "inactive"}
+        ]);
 
-     },
-    {
-        name:"scruber",
-        price:500,
-        catagory:"stationary",
-        status:true
+        // const productSchema = new mongoose.Schema({
+        //     name: String,
+        //     price: Number,
+        //     category: String,
+        //     status: String
+        // }); 
 
-     },
-    ]);
-    const equalQuery = await product.find({status:{$eq:true}});
-    console.log("products which are active",equalQuery);
-    const above45 = await product.find({price:{$gt:45}});
-    console.log(above45);
-    const below45 = await product.find({price:{$lt:45}});
-    console.log(below45);
-    const NP = await product.find({name:{$ne:"scruber"}});
-    console.log(NP);
-    
-    
-    await mongoose.connection.close();
-    console.log("connection closed");
+        // const Product = mongoose.models.Product || mongoose.model("Product",productSchema);
+
+        const equalQuery = await Product.find({status:{$eq:"active"}});
+        // console.log("Products which are active:",equalQuery);
+
+        const greaterQuery = await Product.find({price:{$gt:5000}});
+        console.log("Products which are priced more than 5k:",greaterQuery);
+
+        const lesserQuery = await Product.find({price:{$lt:5000}});
+        console.log("Products which are priced less than 5k:",lesserQuery);
+
+        await mongoose.connection.close();
+        console.log("connection closed");
     }
-     catch(error){
-         console.log("Crud demo error :",error.message);
+    catch(error){
+        console.log("Filter demo error:",error.message);
     }
-    
-    
 }
-runFilterDemo();
+// runFilterDemo();
+
+module.exports = Product;

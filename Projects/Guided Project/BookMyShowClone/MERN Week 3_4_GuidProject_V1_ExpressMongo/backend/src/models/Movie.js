fdfc28271b4b87e248cef18d1553d211ae324ab5
varshithym <ymvarshith@gmail.com>
2026-04-21 12:03:49 +0530

    created movie schema model and exported

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Movie.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "Movie title is required"],
+        trim: true,
+    },
+    genre: {
+        type: String,
+        required: [true, "Genre is required"],
+        enum: [
+            "Action", "Comedy", "Drama", "Horror", "sci-fi", "Romance", "thriller"
+        ],
+        index: true
+    },
+    rating:
+    {
+        type: Number,
+        required: true,
+        min: [1, "Rating must be at least 1"],
+        max: [5, "rating cannot exceed 5"],
+        index: true,
+    },
+    duration: {
+        type: Number,
+        required: [true, "Duration is requried"]
+    },
+    releaseDate: {
+        type: Date,
+        required: [true, "Release date is requried"],
+        index: true,
+    },
+    poster: {
+        type: String,
+        default: "",
+    },
+    language: {
+        type: String,
+        index: true,
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+},
+    {
+        timestamps: true,
+    });
+//Compound index
+movieSchema.index({ genre: 1, rating: -1 });
+
+//Text index
+movieSchema.index({title:"text"});
+
+//virtual field
+movieSchema.virtual("isReleased").get(function(){
+    return this.releaseDate<=new Date();
+});
+
+module.exports = mongoose.model("movie",movieSchema);

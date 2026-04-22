985fc595088eb86f35d4724dea7ae900646133a2
varshithym <ymvarshith@gmail.com>
2026-04-22 15:00:15 +0530

    linked controler function with respective route/request

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/routes/movie.routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const movieController = require("../controllers/movie.controller");
+
+const {protect} =require("../middleware/auth.middleware");
+const {authorize} = require("../middleware/role.middleware");
+// public route 
+router.get("/",movieController.getMovies);
+
+// Admin only route
+router.post("/",protect,authorize("admin"),movieController.createMovie);
+router.put("/:id",protect,authorize("admin"),movieController.updateMovie);
+router.post("/:id",protect,authorize("admin"),movieController.deleteMovie);
+module.exports = router;

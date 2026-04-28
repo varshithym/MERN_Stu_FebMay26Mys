d9c94785de58e97917bdba4d1c5a51401acce7dd
varshithym <ymvarshith@gmail.com>
2026-04-28 09:54:21 +0530

    expression in JSX

--- W9/D1/my-app1/src/App.jsx
+++ W9/D1/my-app1/src/App.jsx
@@ -118,11 +118,22 @@ import './App.css'
 //     </>
 //   )
 // }
-function App() {
+// function App() {
+//             return (
+//                 <div>
+//                     <h1>React Introduction</h1>
+//                     <p>React simplifies UI creation through reusable components</p>
+//                 </div>
+//             );
+//         }
+        function App() {
+            const user = "varshith";
+            const marks = 65;
             return (
                 <div>
-                    <h1>React Introduction</h1>
-                    <p>React simplifies UI creation through reusable components</p>
+                    {/* {curly brackets}: in JSX switch from JSX/HTML-like syntax to JS */}
+                <h1>Hello,{user}</h1>
+                <p>Updated marks: {marks+10}</p>
                 </div>
             );
         }

6e0f53c938068285bb7bd2d60a2dd8ff74c01c7d
varshithym <ymvarshith@gmail.com>
2026-04-29 15:14:18 +0530

    Higher order component

---
+++ W9/D2/prop-example-app/src/components/p12.jsx
@@ -0,0 +1,23 @@
+// Higher order component
+const Greeting = ({name}) => {
+    return <h1>Hello,{name}</h1>
+}
+const WithBorder = (OriginalComponent) =>{
+    return function EnhancedComponent(props){
+        return(
+            <div className="setBorder">
+                <OriginalComponent{...props}/>
+
+            </div>
+        )
+    }
+}
+const GreetingWithBorder = WithBorder(Greeting);
+
+export function HOCExample(){
+    return(
+        <>
+            <GreetingWithBorder name="varshith"/>
+        </>
+    )
+}

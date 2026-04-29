ab73d08b67c9db66fcc95f9cb47759de5f780431
varshithym <ymvarshith@gmail.com>
2026-04-29 10:23:20 +0530

    proped drillinmg example

---
+++ W9/D2/prop-example-app/src/components/P4.jsx
@@ -0,0 +1,36 @@
+import {React} from "react";
+// Prop drilling
+function DrillLayout({user}){
+    return(
+        <div className="layout">
+            <DrillSideBar user={user}/>
+        </div>
+    )
+}
+function DrillSideBar({user}){
+    return(
+        <aside className="sidebar">
+            <DrillUserPanel user={user} />
+        </aside>
+    )
+}
+function DrillUserPanel({user}){
+    return(
+        <section className="user-panel">
+            <ProfileAvatar user={user} />
+        </section>
+    )
+}
+function ProfileAvatar({ user }){
+    return <img src={`/${user.name}.png`} alt={user.name} />
+}
+
+export function PropDrillingDemo(){
+    const user = {name: 'Rakesh'};
+    return(
+        <div>
+            <h3>problem:Prop drilling</h3>
+            <DrillLayout user={user}/>
+        </div>
+    )
+}

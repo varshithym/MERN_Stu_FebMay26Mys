da10972d1af8f5f1afa5b25a211ad20557700e83
varshithym <ymvarshith@gmail.com>
2026-04-29 10:25:20 +0530

    <Container/>,<PropsChildren/>, <PropDrillingDemo/>

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -1,13 +1,21 @@
 
 import './App.css'
 import { PropBasics } from './components/P1'
+import { PropDestructuring } from './components/p2'
+import Container from './components/P3'
+import { PropsChildren } from './components/P3'
+import { PropDrillingDemo } from './components/p4'
 
 function App() {
   
 
   return (
     <>
-      <PropBasics/>
+      {/* <PropBasics/>
+      <PropDestructuring/> */}
+      <Container/>
+      <PropsChildren/>
+      <PropDrillingDemo/>
     </>
   )
 }

3ac547ead980b9aeb5767bf6786be2c1f74aae65
varshithym <ymvarshith@gmail.com>
2026-05-06 13:38:05 +0530

    useReducer hook

--- W10/D2/my-app/src/App.jsx
+++ W10/D2/my-app/src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import { UseContextIntro } from './components/p1'
 import './App.css'
 import { CustomHooksIntro } from './components/p2'
+import { UserRefIntro } from './components/p3'
+import { UseReducerIntro } from './components/p4'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <UseContextIntro/>
+      {/* <UseContextIntro/> */}
       {/* <CustomHooksIntro/> */}
+      {/* <UserRefIntro/> */}
+      <UseReducerIntro/>
       
     </>
   )

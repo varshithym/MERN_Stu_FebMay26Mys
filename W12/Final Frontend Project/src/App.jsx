217bd28cfa5c4dd7e4f95916747607f572da2c1c
varshithym <ymvarshith@gmail.com>
2026-06-12 12:04:40 +0530

    usage of props

--- W12/Final Frontend Project/src/App.jsx
+++ W12/Final Frontend Project/src/App.jsx
@@ -3,10 +3,37 @@ import './App.css'
 import MovieCard from './components/MovieCard'
 import HomePage from './pages/HomePage'
 
+
 function App() {
  
   return (
-      <HomePage/>
+    //   <HomePage/>
+    <span>
+        <MovieCard
+            title="inception"
+            genre="fiction"
+            rating={4.8}
+            duration="2h 28m"
+            poster="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRyuWmayVBvqjd1MxTKpRgauq2cCtUzb7Q9QvaFTkAuxAU_EYMoCE3wBuJeftxIzf0grreIw&s=10"
+        
+        />
+         <MovieCard
+            title="obsession"
+            genre="love"
+            rating={4.8}
+            duration="2h 28m"
+            poster="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTZZmWEhdF2pbwYfOjKBCXSCKXhUQ88EW2NBxSrUfXfBWRchrBN"
+        
+        />
+         <MovieCard
+            title="intesteller"
+            genre="fiction"
+            rating={4.8}
+            duration="2h 28m"
+            poster="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGW7C6dkLMzUE-wYDb0t-Ze6UkSQlRzbkHdFbeuqJDeXCglJqSHLx52z2--O6a-gipTe5aIg&s=10"
+        
+        />
+    </span>
 
   )
 }
--- W12/Final Frontend Project/src/components/MovieCard.jsx
+++ W12/Final Frontend Project/src/components/MovieCard.jsx
@@ -1,19 +1,20 @@
-export default function MovieCard(){
-    const movie = {
-        title:"inception",
-        genre:"fiction",
-        rating:"9",
-        duration:"2h 28m"
-    };
+export default function MovieCard(props){
+    // const movie = {
+    //     title:"inception",
+    //     genre:"fiction",
+    //     rating:"9",
+    //     duration:"2h 28m"
+    // };
     return(
         <div className="movie-card">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRyuWmayVBvqjd1MxTKpRgauq2cCtUzb7Q9QvaFTkAuxAU_EYMoCE3wBuJeftxIzf0grreIw&s=10" alt={movie.title} style={{height:400}}  />
-            <h2>{movie.title}</h2>
-            <p>Genre:{movie.genre}</p>
-            <p>rating:{movie.rating}</p>
-            <p>Duration:{movie.duration}</p>
-            <button>Book Now</button>
+           <h3>{props.title}</h3>
+           <img src={props.poster} alt={props.title} />
+           <h3>{props.title}</h3>
+           <p>Genre:{props.genre}</p>
+           <p>rating:{props.rating}</p>
+           <p>Duration:{props.duration}</p>
+           <button>Book Now</button>
 
         </div>
-    )
+    );
 }
